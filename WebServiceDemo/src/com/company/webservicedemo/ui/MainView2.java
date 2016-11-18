a2fd733e7304feff191f3adcda30d0e96aa977c4
ES-KS <es-ks@trash-mail.de>
2016-11-18 14:34:59 +0100

    Feature 777 implemented

--- WebServiceDemo/src/com/company/webservicedemo/ui/MainView2.java
+++ WebServiceDemo/src/com/company/webservicedemo/ui/MainView2.java
@@ -2,6 +2,7 @@ package com.company.webservicedemo.ui;
 
 import com.xdev.ui.XdevGridLayout;
 import com.xdev.ui.XdevView;
+import com.xdev.ui.entitycomponent.table.XdevTable;
 
 public class MainView2 extends XdevView {
 
@@ -20,13 +21,22 @@ public class MainView2 extends XdevView {
 	// <generated-code name="initUI">
 	private void initUI() {
 		this.gridLayout = new XdevGridLayout();
-
+		this.table = new XdevTable<>();
+	
+		this.gridLayout.setColumns(1);
+		this.gridLayout.setRows(1);
+		this.table.setSizeFull();
+		this.gridLayout.addComponent(this.table, 0, 0);
+		this.gridLayout.setColumnExpandRatio(0, 100.0F);
+		this.gridLayout.setRowExpandRatio(0, 100.0F);
 		this.gridLayout.setSizeFull();
 		this.setContent(this.gridLayout);
 		this.setSizeFull();
 	} // </generated-code>
 
 	// <generated-code name="variables">
+	private XdevTable<?> table;
 	private XdevGridLayout gridLayout; // </generated-code>
+
 
 }
